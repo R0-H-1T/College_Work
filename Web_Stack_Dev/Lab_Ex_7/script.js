f5b9fa7ccfd8279e8a9fbf15e13b94199801e3c0
Rohit Tank <90775575+R0-H-1T@users.noreply.github.com>
2023-09-17 11:15:42 +0530

    Lab 7 in progress

--- Web_Stack_Dev/Lab_Ex_7/script.js
+++ Web_Stack_Dev/Lab_Ex_7/script.js
@@ -1,1 +1,30 @@
-// to be done 
+
+
+
+let booklist = document.getElementById('bookList');
+booklist.addEventListener('click', () => {
+    
+})
+
+
+// const booksurl = "https://openlibrary.org/works/OL45804W.json"
+// const books = fetch(booksurl);
+// books
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .catch(handleError);
+
+
+async function logMovies() {
+    const response = await fetch("https://raw.githubusercontent.com/benoitvallon/100-best-books/master/books.json");
+    const movies = await response.json();
+    movies.forEach(function(item) {
+        console.log(item.author);
+    });
+}
+
+  logMovies()
+
+
+  
