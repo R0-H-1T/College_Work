ed7d34ae8c780f6365e7aa9eb6e1b8daf08f3126
Rohit Tank <90775575+R0-H-1T@users.noreply.github.com>
2023-09-22 15:08:41 +0530

    Lab 8 - product displaying

--- Web_Stack_Dev/Lab_Ex_8/script.js
+++ Web_Stack_Dev/Lab_Ex_8/script.js
@@ -7,36 +7,41 @@ var productTags = {
     "men's clothing": 'mensclothing'
 }
 
-
+var productsList = {};
+ 
 const fetchHandler = async() => {
-    console.log(await fetch('https://fakestoreapi.com/products/categories').then(res => res.json()))
-    for(let i=0; i<10; i++){
+    // console.log(await fetch('https://fakestoreapi.com/products/categories').then(res => res.json()))
+    document.getElementById('demo').innerHTML = '';
+    var count = 1 ;
+    for(let i=0; i<10; i++, count++){
         var div = document.createElement('div');
         const randomNumber = Math.floor(Math.random() * 20) + 1
         var product = await fetch(`https://fakestoreapi.com/products/${randomNumber}`)
         product = await product.json()
-        console.log(productTags[product.category])
+        productsList[productTags[product.category]+ count.toString()] = product;
         div.innerHTML = `
-                    <div class="card mb-3" style="max-width: 540px;">
+                    <div class="card mb-3" style="max-width: 90%;">
                       <div class="row g-0">
                         <div class="col-md-4">
                           <img src="${product.image}" class="img-fluid rounded-start" alt="...">
-                        </div>
+                        </div> 
                         <div class="col-md-8">
                           <div class="card-body">
                             <h5 class="card-title">${product.title}</h5>
                             <p class="card-text">${product.description}</p>
                             <p class="card-text">$${product.price}</p>
-                            <a href="#">#${productTags[product.category]}</a>
+                            Tag: <a href="#">#${productTags[product.category]+ count.toString()}</a>
                           </div>
                         </div>
                       </div>
                     </div>
         `;
-        console.log(product.category)
         document.getElementById('demo').appendChild(div);
         // randomNumbers.push(Math.floor(Math.random() * 20) + 1);
     }
+    console.log(productsList)
+
+    // cownsole.log(productsList[0]);
     // var categories = await fetch('https://fakestoreapi.com/products')
     // var categoryProducts = await fetch('https://fakestoreapi.com/products/category/jewelery?limit=3')
     // categories = await categories.json()
@@ -44,6 +49,47 @@ const fetchHandler = async() => {
     // console.log(categories.length, categoryProducts)
 }
 
+ 
+const searchProduct = () => {
+  const searchProduct = document.getElementById('inputProd').value;
+  if(searchProduct == ''){
+    document.getElementById('search-res').textContent = 'Nothing entered :)';
+  }else{
+    document.getElementById('demo2').innerHTML = '';
+    document.getElementById('search-res').textContent = '';
+    var div = document.createElement('div');
+    const product =  productsList[searchProduct];
+    if(product == undefined){
+      document.getElementById('demo2').innerHTML = `<h4>No products found.</h4>`
+    }
+    if(product){
+        div.innerHTML = `
+        <div class="card mb-3" style="max-width: 90%;>
+          <div class="row g-0">
+            <div class="col-md-4">
+              <img src="${product.image}" class="img-fluid rounded-start" style="max-width:300px;" alt="...">
+            </div> 
+            <div class="col-md-8">
+              <div class="card-body">
+                <h5 class="card-title">${product.title}</h5>
+                <p class="card-text">${product.description}</p>
+                <p class="card-text">$${product.price}</p>
+              </div>
+            </div>
+          </div>
+        </div>`
+        document.getElementById('demo2').appendChild(div);
+    }
+
+  }
+}
 
 
 document.getElementById('fetchProducts').addEventListener('click',fetchHandler);
+document.getElementById('searchProduct').addEventListener('click', searchProduct);
+
+function clearFields(){
+  document.getElementById('inputProd').value = '';
+}
+
+window.onload = clearFields;
